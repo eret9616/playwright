d6b3f5426001af2176a6f665b247163bb58ba6ef
hh7 <hh7@xm.com>
2025-12-17 13:47:27 +0800

    chore: simplify shouldOpen condition in HtmlReporter

--- packages/playwright/src/reporters/html.ts
+++ packages/playwright/src/reporters/html.ts
@@ -150,7 +150,7 @@ class HtmlReporter implements ReporterV2 {
     if (process.env.CI || !this._buildResult)
       return;
     const { ok, singleTestId } = this._buildResult;
-    const shouldOpen = !!process.stdin.isTTY && (this._open === 'always' || (!ok && this._open === 'on-failure'));
+    const shouldOpen = (this._open === 'always' || (!ok && this._open === 'on-failure'));
     if (shouldOpen) {
       await showHTMLReport(this._outputFolder, this._host, this._port, singleTestId);
     } else if (this._options._mode === 'test' && !!process.stdin.isTTY) {
