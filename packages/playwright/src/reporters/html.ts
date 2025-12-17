9203a9adc8eec8e4787ff45859a7d204e69a11df
hh7 <hh7@xm.com>
2025-12-17 13:47:27 +0800

    chore: simplify shouldOpen condition in HtmlReporter

--- packages/playwright/src/reporters/html.ts
+++ packages/playwright/src/reporters/html.ts
@@ -159,7 +159,7 @@ class HtmlReporter implements ReporterV2 {
       return;
     const { ok, singleTestId } = this._buildResult;
     const isCodingAgent = !!process.env.CLAUDECODE || !!process.env.COPILOT_CLI;
-    const shouldOpen = !isCodingAgent && !!process.stdin.isTTY && (this._open === 'always' || (!ok && this._open === 'on-failure'));
+    const shouldOpen = !isCodingAgent && (this._open === 'always' || (!ok && this._open === 'on-failure'));
     if (shouldOpen) {
       await showHTMLReport(this._outputFolder, this._host, this._port, singleTestId);
     } else if (this._options._mode === 'test' && !!process.stdin.isTTY) {
