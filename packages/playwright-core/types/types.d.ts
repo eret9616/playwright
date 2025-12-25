7d35fd7f5d8866ad761883e05ab737adc30cf15c
eret9616 <947416983@qq.com>
2025-12-25 15:44:52 +0800

    feat: 添加小米设备描述符和 prettier 配置

--- packages/playwright-core/types/types.d.ts
+++ packages/playwright-core/types/types.d.ts
@@ -23862,6 +23862,8 @@ type Devices = {
   "Pixel 7 landscape": DeviceDescriptor;
   "Moto G4": DeviceDescriptor;
   "Moto G4 landscape": DeviceDescriptor;
+  "Xiaomi 13": DeviceDescriptor;
+  "Xiaomi 13 landscape": DeviceDescriptor;
   "Desktop Chrome HiDPI": DeviceDescriptor;
   "Desktop Edge HiDPI": DeviceDescriptor;
   "Desktop Firefox HiDPI": DeviceDescriptor;
